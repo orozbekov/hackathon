511dde1d679e2738dcca042e5fd0837c7f53a6cd
nagadzu <nagadzu@mail.ru>
2022-05-01 14:51:49 +0600

    updated my_app

--- apps/my_app/admin.py
+++ apps/my_app/admin.py
@@ -1,49 +1,39 @@
 from django.contrib import admin
 from . import models
 
-@admin.register(models.Category)
 
+@admin.register(models.Category)
 class CatAdmin(admin.ModelAdmin):
-	list_display = [
-        'name',
-        ]
+    list_display = [
+        "name",
+    ]
+
 
 @admin.register(models.Quizzes)
-
 class QuizAdmin(admin.ModelAdmin):
-	list_display = [
-        'id', 
-        'title',
-        ]
+    list_display = [
+        "id",
+        "title",
+    ]
+
 
 class AnswerInlineModel(admin.TabularInline):
     model = models.Answer
-    fields = [
-        'answer_text', 
-        'is_right'
-        ]
+    fields = ["answer_text", "is_right"]
+
 
 @admin.register(models.Question)
-
 class QuestionAdmin(admin.ModelAdmin):
     fields = [
-        'title',
-        'quiz',
-        ]
-    list_display = [
-        'title', 
-        'quiz',
-        'date_updated'
-        ]
+        "title",
+        "quiz",
+    ]
+    list_display = ["title", "quiz", "date_updated"]
     inlines = [
-        AnswerInlineModel, 
-        ] 
+        AnswerInlineModel,
+    ]
+
 
 @admin.register(models.Answer)
-
 class AnswerAdmin(admin.ModelAdmin):
-    list_display = [
-        'answer_text', 
-        'is_right', 
-        'question'
-        ]
+    list_display = ["answer_text", "is_right", "question"]
--- hackathon/settings.py
+++ hackathon/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3_2',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
